33f2e4f62769b05ecd7e9c4809047dd597b577d0
IT-Professional-Kristina <157192330+IT-Professional-Kristina@users.noreply.github.com>
2025-10-05 21:16:46 -0400

    Create runtime_Error Divisible by zero.c

---
+++ runtime_Error.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 5;
+    int b = 0;
+
+    int result;
+    result = a / b;
+
+    printf("Result: %d\n", result);
+    return 0;
+
+/* runtime error program compiles but crashes while running A runtime error occurs while the program is running — after successful compilation.
+That means:
+
+The program compiles (no syntax errors).
+
+It starts running.
+
+But something goes wrong during execution — like dividing by zero or accessing invalid memory. */
+
+}
