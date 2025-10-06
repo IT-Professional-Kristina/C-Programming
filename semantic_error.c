26fb734d4ab7000d25ed61ee7e16498bc729972b
IT-Professional-Kristina <157192330+IT-Professional-Kristina@users.noreply.github.com>
2025-10-05 21:02:05 -0400

    Create semantic_error.c
    semantic error - program compiles but meaning is wrong

---
+++ semantic_error.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 45;
+    int b = 34;
+    int sum;
+    sum = a - b;
+
+    printf("%d", sum);
+    return 0;
+
+    }
+// This meaning is wrong it is supposed to be a + b not a - b
+
