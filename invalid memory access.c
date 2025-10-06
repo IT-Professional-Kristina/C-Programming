08eae5ce2d7221ed4b87387866ed10f82258faa4
IT-Professional-Kristina <157192330+IT-Professional-Kristina@users.noreply.github.com>
2025-10-05 21:34:03 -0400

    Create invalid memory access.c

---
+++ invalid memory access.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[3] = {1, 2, 3};
+
+    printf("%d\n", numbers[5]);
+    return 0;
+
+
+// invalid memory access trying to read memory that doesn't belong
+}
