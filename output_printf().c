b9d8d2eb74c874c32d3958829df6a2733483ee1d
IT-Professional-Kristina <157192330+IT-Professional-Kristina@users.noreply.github.com>
2025-10-07 21:27:06 -0400

    Create output_printf().c
    Learning the output function printf()

---
+++ output_printf().c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float temp = 36.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Temperature: 36.5%cC\n", 248);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+
+
+
+}
