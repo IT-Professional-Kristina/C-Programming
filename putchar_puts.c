c6775cd01a63713b0d90f5deafca46d4aeba93cb
IT-Professional-Kristina <157192330+IT-Professional-Kristina@users.noreply.github.com>
2025-10-07 21:33:40 -0400

    Create putchar_puts.c
    output functions

---
+++ putchar_puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// unformatted output to screen
+int main() {
+//putchar prints one character at a time
+putchar('K');
+putchar('L');
+putchar ('B');
+putchar('\n'); // newline
+
+// puts prints an entire string and automatically adds a new line
+puts("Hello, World!");
+puts("Learning C programming language!");
+
+return 0;
+
+
+
+
+}
